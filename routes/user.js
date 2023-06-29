7116c35860a83f3261c122e399b61b0801dab48d
jchampol <jchampol@etu.u-bordeaux.fr>
2023-06-29 11:38:28 +0200

    Retrait route de test, vérification déploiement

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require(`../controllers/user`);
 
 router.get('/', userController.getUsers);
-router.get('/test', userController.getUsers);
 router.get('/:id', userController.getUser);
 router.post('/', userController.createUser);
 router.put('/:id', userController.updateUser);
