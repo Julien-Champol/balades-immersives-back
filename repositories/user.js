703717a22a59feb2e80bf5445736febc509722e2
Maxime <gelindo33@gmail.com>
2023-07-04 17:07:38 +0200

    route connexion

--- repositories/user.js
+++ repositories/user.js
@@ -4,6 +4,9 @@ exports.getUsers = () => User.find({});
 
 exports.getUserById = (id) => User.findById(id);
 
+exports.getUserConnexion = (email,password) => User.find({ email: { $in: email }, password: { $in: password } });
+
+
 exports.createUser = (userData) => User.create(userData);
 
 exports.updateUser = (id, userData) => User.findByIdAndUpdate(id, userData, { new: true});
