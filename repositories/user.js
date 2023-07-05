f4792babb760268055bb6d94d92f616487cb0acf
Your Name <you@example.com>
2023-07-05 17:32:19 +0200

    fix

--- repositories/user.js
+++ repositories/user.js
@@ -5,17 +5,17 @@ exports.getUsers = () => User.find({});
 exports.getUserById = (id) => User.findById(id);
 
 exports.createUser = async (userData) => {
-    const isEmailTaken = await User.exists(userData.email);
+    const isEmailTaken = await User.exists({email: {$in: userData.email}});
     if (isEmailTaken) {
         throw new Error('L\'adresse e-mail est déjà utilisée.');
+    } else {
+        await User.create(userData);
     }
-    await User.create(userData);
 };
 
-exports.getUserConnexion = (email) => User.find({ email: { $in: email } });
+exports.getUserConnexion = (email) => User.find({email: {$in: email}});
 
 
-
-exports.updateUser = (id, userData) => User.findByIdAndUpdate(id, userData, { new: true});
+exports.updateUser = (id, userData) => User.findByIdAndUpdate(id, userData, {new: true});
 
 exports.deleteUser = (id) => User.findByIdAndDelete(id);
